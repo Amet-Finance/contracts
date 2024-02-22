b5797c0bd03dab5c95dedbb2e2e532bbd4701333
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-02-23 02:04:04 +0400

    update

--- contracts/fixed-flex/interfaces/IBond.sol
+++ contracts/fixed-flex/interfaces/IBond.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/// @title IBond Interface
+/// @notice Interface for the Bond contract managing the financial aspects of bonds
 interface IBond {
-    function getSettledPurchaseDetails() external view returns (IERC20, uint256);
+    /// @notice Retrieves the purchase token and amount details for the bond if it is fully settled
+    /// @return purchaseToken The ERC20 token used for bond purchases if the bond is fully settled
+    /// @return purchaseAmount The amount of the purchase token required to buy the bond if it is fully settled
+    function getSettledPurchaseDetails() external view returns (IERC20 purchaseToken, uint256 purchaseAmount);
 }
--- contracts/fixed-flex/interfaces/IVault.sol
+++ contracts/fixed-flex/interfaces/IVault.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Types} from "../libraries/Types.sol";
+import { Types } from "../libraries/Types.sol";
 
+/// @title IVault Interface
+/// @notice Interface for the Vault contract managing bonds
 interface IVault {
+    
+    /// @notice Initializes a bond with default fee settings
+    /// @dev This function should be callable only by the issuer contract
+    /// @param bondAddress The address of the bond to initialize
     function initializeBond(address bondAddress) external payable;
+
+    /// @notice Retrieves the fee details for a specific bond
+    /// @param bondAddress The address of the bond
+    /// @return Bond fee details including purchase, early redemption, and referral reward rates
     function getBondFeeDetails(address bondAddress) external returns (Types.BondFeeDetails calldata);
+
+    /// @notice Records a referral purchase for a bond
+    /// @param operator The address of the operator performing the purchase
+    /// @param referrer The address of the referrer
+    /// @param quantity The quantity of bonds purchased
     function recordReferralPurchase(address operator, address referrer, uint40 quantity) external;
 }
